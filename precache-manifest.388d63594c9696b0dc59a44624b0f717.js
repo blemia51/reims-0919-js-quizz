c606d245f583861f1e47cf27b4cf0f5400f39837
Herve Bourelle <herve.bourelle@gmail.com>
2020-10-15 15:53:13 +0200

    Updates

---
+++ precache-manifest.388d63594c9696b0dc59a44624b0f717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e4945ad3de62d88029d13ec88c250b",
+    "url": "/reims-0919-js-quizz/index.html"
+  },
+  {
+    "revision": "09f11ed2b976b0a7304e",
+    "url": "/reims-0919-js-quizz/static/css/main.71ed8b4f.chunk.css"
+  },
+  {
+    "revision": "50ea18ba246b0fbe7484",
+    "url": "/reims-0919-js-quizz/static/js/2.89284974.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reims-0919-js-quizz/static/js/2.89284974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09f11ed2b976b0a7304e",
+    "url": "/reims-0919-js-quizz/static/js/main.882fd6a7.chunk.js"
+  },
+  {
+    "revision": "75323f4d3f1fddc7408a",
+    "url": "/reims-0919-js-quizz/static/js/runtime-main.61a8c461.js"
+  }
+]);
