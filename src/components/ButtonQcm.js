309a1917bc08c26a9c48ff2039d82f1d092fd035
Herve Bourelle <herve.bourelle@gmail.com>
2020-10-15 11:00:04 +0200

    refacto 003

--- src/components/ButtonQcm.js
+++ src/components/ButtonQcm.js
@@ -1,91 +1,124 @@
-import React from "react"
-import './ButtonQcm.css'
+import React from "react";
+import "./ButtonQcm.css";
 
+class ButtonQcm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      win: false,
+      lost: false,
+      buttonWinOrder: "",
+    };
+    this.nextQuestion = this.nextQuestion.bind(this);
+    this.checkWin = this.checkWin.bind(this);
+    this.checkLost = this.checkLost.bind(this);
+    this.buttonWinOrder = this.buttonWinOrder.bind(this);
+  }
 
-    class ButtonQcm extends React.Component {
-        constructor(props) {
-            super(props)
-            this.state = {
-                win: false,
-                lost: false,
-                buttonWinOrder: ''
+  buttonWinOrder() {
+    this.setState({
+      buttonWinOrder: Math.floor(Math.random() * Math.floor(4) + 1),
+    });
+  }
+
+  checkWin() {
+    this.setState({ win: true });
+    this.props.incrementScore();
+  }
+
+  checkLost() {
+    this.setState({ lost: true });
+  }
+
+  nextQuestion() {
+    this.setState({ win: false, lost: false });
+    this.props.getQuestions();
+    this.props.incrementQuestionNumber();
+    this.buttonWinOrder();
+  }
+
+  componentDidMount() {
+    this.buttonWinOrder();
+  }
+
+  render() {
+		const { win, lost, buttonWinOrder } = this.state;
+		const { correct_answer, incorrect_answer } = this.props;
+    return (
+      <div className="cardContent">
+        <div className="button-order">
+          <button
+            onClick={this.checkWin}
+            style={{ order: buttonWinOrder }}
+            className={
+              win || lost ? "winButton" : "qcmButton"
             }
-            this.nextQuestion = this.nextQuestion.bind(this)
-            this.checkWin = this.checkWin.bind(this)
-            this.checkLost = this.checkLost.bind(this)
-            this.buttonWinOrder = this.buttonWinOrder.bind(this)
-        }
+          >
+            {correct_answer
+              .replace(/&quot;|&#039;/g, "'")
+              .replace(/amp;/g, "")
+              .replace(/&eacute;/g, "é")}
+          </button>
 
-        buttonWinOrder() {
-            this.setState({
-                buttonWinOrder: Math.floor(Math.random()*Math.floor(4)+1) 
-            })
-        }
+          <button
+            onClick={this.checkLost}
+            className={lost ? "lostButton" : "qcmButton"}
+          >
+            {incorrect_answer[0]
+              .replace(/&quot;|&#039;/g, "'")
+              .replace(/amp;/g, "")
+              .replace(/&eacute;/g, "é")}
+          </button>
 
-        checkWin() {
-            this.setState({win: true});
-            this.props.incrementScore()
-        }
+          <button
+            onClick={this.checkLost}
+            className={lost ? "lostButton" : "qcmButton"}
+          >
+            {incorrect_answer[1]
+              .replace(/&quot;|&#039;/g, "'")
+              .replace(/amp;/g, "")
+              .replace(/&eacute;/g, "é")}
+          </button>
 
-        checkLost() {
-            this.setState({lost: true})
-        }
+          <button
+            onClick={this.checkLost}
+            className={lost ? "lostButton" : "qcmButton"}
+          >
+            {incorrect_answer[2]
+              .replace(/&quot;|&#039;/g, "'")
+              .replace(/amp;/g, "")
+              .replace(/&eacute;/g, "é")}
+          </button>
+        </div>
 
-        nextQuestion() {
-            this.setState({win: false, lost :false});
-            this.props.getQuestions();
-            this.props.incrementQuestionNumber();
-            this.buttonWinOrder();
-          }
+        <div className={win ? "imageWin" : "noImage"}>
+          <img
+            className="winLogo"
+            src={"https://i.ibb.co/Pzgh2rx/good-answer-smiley.png"}
+            alt="Win"
+          ></img>
+        </div>
 
-        componentDidMount() {
-            this.buttonWinOrder();
-            } 
-
-    render() {
-    return (   
-        
-    <div className='cardContent'>
-  
-        <div className= 'button-order'>
-            <button
-                onClick={this.checkWin}
-                style={{ order: this.state.buttonWinOrder }}
-                className={this.state.win||this.state.lost?'winButton':'qcmButton'}>
-                {this.props.correct_answer.replace(/&quot;|&#039;/g,"'").replace(/amp;/g,'').replace(/&eacute;/g,'é')}
-            </button>
-
-            <button onClick={this.checkLost} 
-                className={this.state.lost?'lostButton':'qcmButton'}>
-                {this.props.incorrect_answer[0].replace(/&quot;|&#039;/g,"'").replace(/amp;/g,'').replace(/&eacute;/g,'é')}
-            </button>
-        
-            <button onClick={this.checkLost}
-                className={this.state.lost?'lostButton':'qcmButton'}>
-                {this.props.incorrect_answer[1].replace(/&quot;|&#039;/g,"'").replace(/amp;/g,'').replace(/&eacute;/g,'é')}
-            </button>
-
-            <button onClick={this.checkLost}
-            
-                className={this.state.lost?'lostButton':'qcmButton'}>
-                {this.props.incorrect_answer[2].replace(/&quot;|&#039;/g,"'").replace(/amp;/g,'').replace(/&eacute;/g,'é')}
-            </button>
-
-        </div> 
-
-        <div className={this.state.win?'imageWin':'noImage'} >
-             <img className='winLogo' src={"https://i.ibb.co/Pzgh2rx/good-answer-smiley.png"} alt="Win" ></img>
-             
-        </div> 
-
-        <div className={this.state.lost?'imageLost':'noImage'}>
-             <img className='lostLogo' src={"https://i.ibb.co/3BkyqKX/bad-answer-smiley.png"} alt="Lost" ></img> 
-             
-        </div> 
-        <div><button className={this.state.lost||this.state.win?'qcmButton':'noImage'} onClick={this.nextQuestion}>Next question</button></div>    
-    </div>
-    )   
- }
+        <div className={lost ? "imageLost" : "noImage"}>
+          <img
+            className="lostLogo"
+            src={"https://i.ibb.co/3BkyqKX/bad-answer-smiley.png"}
+            alt="Lost"
+          ></img>
+        </div>
+        <div>
+          <button
+            className={
+              lost || win ? "qcmButton" : "noImage"
+            }
+            onClick={this.nextQuestion}
+          >
+            Next question
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ButtonQcm;
