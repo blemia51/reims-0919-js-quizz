90f5c51d9e05b3f769a056ce849b86411a43e5c5
Herve Bourelle <herve.bourelle@gmail.com>
2020-10-15 14:35:49 +0200

    change route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import Routes from './components/Routes';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ListCategory from "./components/ListCategory";
+import ChoixQfmVF from "./components/ChoixQfmVF";
+import CardQuestion from "./components/CardQuestion";
+import Nav from "./components/Nav";
+import CardQuestionBool from "./components/CardQuestionBool";
 
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App"> 
-        <Routes/>
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ListCategory} />
+
+            <Route
+              path="/choiceQuestion"
+              render={(props) => (
+                <>
+                  <Nav />
+                  <ChoixQfmVF {...props} />
+                </>
+              )}
+            />
+
+            <Route
+              path="/question"
+              render={(props) => (
+                <>
+                  <Nav />
+                  <CardQuestion {...props} />
+                </>
+              )}
+            />
+
+            <Route
+              path="/questionbool"
+              render={(props) => (
+                <>
+                  <Nav />
+                  <CardQuestionBool {...props} />
+                </>
+              )}
+            />
+          </Switch>
+        </Router>
       </div>
     );
   }
